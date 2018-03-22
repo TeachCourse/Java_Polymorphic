4713173557fa42c089ec2cf41a553c26c9d81316
Administrator <123456>
2018-03-22 22:26:29 +0800

    init

--- src/cn/teachcourse/PolymorphicTest.java
+++ src/cn/teachcourse/PolymorphicTest.java
@@ -1,5 +1,8 @@
 package cn.teachcourse;
 
+/**
+ * Created by http://teachcourse.cn on 2018/03/22.
+ */
 public class PolymorphicTest {
 
 	/**
@@ -13,18 +16,19 @@ public class PolymorphicTest {
 		C c = new C();
 		D d = new D();
 
-		System.out.println("1--" + a1.show(b));//打印：1--A and A
-		System.out.println("2--" + a1.show(c));//打印：2--A and A
-		System.out.println("3--" + a1.show(d));//打印：3--A and D
-		
-		System.out.println("4--" + a2.show(b));//打印：4--B and A
-		System.out.println("5--" + a2.show(c));//打印：5--B and A
-		System.out.println("6--" + a2.show(d));//打印：6--A and D
-		
-		System.out.println("7--" + b.show(b));//打印：7--B and B
-		System.out.println("8--" + b.show(c));//打印：8--B and B
-		System.out.println("9--" + b.show(d));//打印：9--A and D
+		System.out.println("1--" + a1.show(b));// 打印：1--A and A
+		System.out.println("2--" + a1.show(c));// 打印：2--A and A
+		System.out.println("3--" + a1.show(d));// 打印：3--A and D
+
+		System.out.println("4--" + a2.show(b));// 打印：4--B and A
+		System.out.println("5--" + a2.show(c));// 打印：5--B and A
+		System.out.println("6--" + a2.show(d));// 打印：6--A and D
+
+		System.out.println("7--" + b.show(b));// 打印：7--B and B
+		System.out.println("8--" + b.show(c));// 打印：8--B and B
+		System.out.println("9--" + b.show(d));// 打印：9--A and D
 	}
+
 	static class A {
 		public String show(D obj) {
 			return ("A and D");
@@ -46,11 +50,11 @@ public class PolymorphicTest {
 	}
 
 	static class C extends B {
-	
+
 	}
 
 	static class D extends B {
-	
+
 	}
 
 }
